96895e0f4f3eafd8f1d668755f0cda47514f6257
Pete Dibdin <pete@dibdin.me>
2021-10-29 20:52:34 +0100

    added ability to confirm the choice

--- src/OptionSelectModal.js
+++ src/OptionSelectModal.js
@@ -1,8 +1,43 @@
 import React, {Component} from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import autoBindReact from "auto-bind";
 
 class OptionSelectModal extends Component {
+
+  static initialState = {
+    selectedOption: -1,
+    showConfirmation: false
+  } 
+
+  constructor(props) {
+    super(props);
+    autoBindReact(this);
+
+    this.state = OptionSelectModal.initialState;
+  }
+
+  optionClicked(item, index) {
+    let stateUpdate = {};
+    stateUpdate.selectedOption = index;
+    stateUpdate.showConfirmation = this.props.requireConfirmation; 
+    this.setState(stateUpdate);
+
+    if (!this.props.requireConfirmation) {
+      this.props.onOptionSelect(item);
+      this.props.onHide();
+    }
+  }
+
+  okClicked() {
+    this.setState(OptionSelectModal.initialState);
+    this.props.onOptionSelect(this.props.options[this.state.selectedOption]);
+  }
+
+  cancelClicked() {
+    this.setState(OptionSelectModal.initialState);
+    this.props.onHide();
+  }
 
   render() {
     return (
@@ -22,12 +57,21 @@ class OptionSelectModal extends Component {
           <p>
             {this.props.message}
           </p>
-        </Modal.Body>
-        <Modal.Footer>
           {this.props.options.map((item, index) => (
-            <Button key={item} onClick={() => this.props.onOptionSelect(item)}>{item}</Button>
+            <Button
+              key={item} 
+              className="m-1" 
+              variant={(this.state.selectedOption === index) ? "primary" : "outline-primary"} 
+              onClick={() => this.optionClicked(item, index)}>{item}</Button>
           ))}
-        </Modal.Footer>
+        </Modal.Body>
+        {this.state.showConfirmation &&
+          <Modal.Footer>
+            {this.props.confirmationMessage}
+            <Button onClick={() => this.okClicked()}>Ok</Button>
+            <Button onClick={() => this.cancelClicked()}>Cancel</Button>
+          </Modal.Footer>
+        }
       </Modal>
     );
   }
